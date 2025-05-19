0ef6146cb9ec10ea9b78eac2d1af90f83cd92078
Vitor0389 <vitor0389@gmail.com>
2025-05-19 17:07:54 -0300

    feat : adicionado CadastroActivity e UI da main
    feat : criado o controller de cadastro, com as variaveis que vão receber os dados da UI.
    feat: adicionado AppCompat()

---
+++ app/src/main/java/com/personaltasks/controller/CadastroActivity.kt
@@ -0,0 +1,11 @@
+package com.personaltasks.controller
+import java.util.UUID
+
+class CadastroActivity {
+    private lateinit var editTextNome: EditText
+    private lateinit var editTextDescricao: EditText
+    private lateinit var textDataEscolhida: TextView
+    private lateinit var buttonDatePicker: Button
+    private lateinit var buttonSalvar: Button
+    private lateinit var buttonCancelar: Button
+}
