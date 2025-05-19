b2b917cc2af4e15ac19e00c4b68b7bbe60e74e72
Vitor0389 <vitor0389@gmail.com>
2025-05-19 18:06:33 -0300

    feat : criada a data class que representa a tarefa no banco de dados

---
+++ app/src/main/java/com/personaltasks/model/Tarefa.kt
@@ -0,0 +1,12 @@
+package com.personaltasks.model
+
+import java.io.Serializable
+import java.util.*
+
+// Representa a tabela no banco de dados
+data class Tarefa(
+    val id: UUID,
+    val nome: String,
+    val descricao: String,
+    val data: Date
+) : Serializable
