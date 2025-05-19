e57558562b9186bcae3603a19150927e514683a6
Vitor0389 <vitor0389@gmail.com>
2025-05-19 18:31:38 -0300

    feat : conectando adapter com a tela principal

--- app/src/main/java/com/personaltasks/controller/MainActivity.kt
+++ app/src/main/java/com/personaltasks/controller/MainActivity.kt
@@ -1,14 +1,49 @@
 package com.personaltasks.controller
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.personaltasks.R
+import com.personaltasks.adapter.TarefaAdapter
+import com.personaltasks.model.Tarefa
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+           * Cria a lista tarefas e passa para o TarefaAdapter
+           * RecycleView usa esse adapter p mostrar cada tarefa usando o layout do item_tarefa.xml
+           * Quando o usuario cria na CadastroActivity ela é retornada para a Main
+           * Adiciona a tarefa na lista e "avisa" o adapter para atualizar na tela
+
+         */
+        val tarefas = mutableListOf<Tarefa>()
+        val adapter = TarefaAdapter(tarefas)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewTarefas);
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val tarefa = result.data?.getSerializableExtra("tarefa") as? Tarefa
+                tarefa?.let {
+                    tarefas.add(it)
+                    adapter.notifyItemInserted(tarefas.size - 1)
+                }
+            }
+        }
+
+        findViewById<Button>(R.id.botaoCadastro).setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
+            launcher.launch(intent)
+        }
 
         }
     }
